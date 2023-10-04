ff124215127d1d3e1c9d7a9c06cac78f3594f2ba
Łukasze <lukaszkucharzyk17@gmail.com>
2023-10-04 18:05:36 +0200

    start setup proxy to handle cors

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = 8000;
+const HOST = "localhost";
+
+const API_URL = "https://api.igdb.com/";
+
+app.use(cors());
+
+app.get("/products/:id", function (req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(PORT, function () {
+  console.log("CORS-enabled web server listening on port" + PORT);
+});
+
+app.get("/status", (req, res, next) => {
+  res.send("This is a proxy service");
+});
+
+const proxyOptions = {
+  target: API_URL,
+  changeOrigin: true,
+  headers: {
+    Accept: "application/json",
+    "Client-ID": process.env.CLIENT_ID,
+    Authorization: process.env.AUTH,
+    body: "game,name",
+  },
+};
+
+const proxy = createProxyMiddleware(proxyOptions);
+
+app.use("/v4", proxy);
+
+app.listen(PORT, HOST, () => {
+  console.log(`Proxy Started at ${HOST}:${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ import GameDetails from "./components/GameDetails";
 
 const APIKey = "B143DB1BFAD78F22D9EC882124CD634E";
 
-//http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=B143DB1BFAD78F22D9EC882124CD634E&format=json
-//https://store.steampowered.com/api/appdetails?appids=1029780
-//https://api.steampowered.com/ISteamNews/GetNewsForApp/v2?appid=1029780
-
 function App() {
-  const [savedGameIDs, setSavedGameIDs] = useState([1426450, 1029780, 1145360]);
+  const [savedGameIDs, setSavedGameIDs] = useState();
   const [savedGameData, setSavedGameData] = useState([]);
 
   useEffect(() => {
-    if (savedGameIDs) {
-      async function fetchLastUpadeDates() {
-        const newSavedGamesData = [];
-        savedGameIDs.forEach((game) => {});
+    if (true) {
+      async function fetchGamesData() {
+        // const newSavedGamesData = [];
+        //for (const game of savedGameIDs) {
+        const res = await fetch(`http://localhost:8000/v4/search`, {
+          mode: "cors",
+          method: "GET",
+        });
+        const data = await res.json();
+        console.log(data);
+        //}
       }
+      // async function fetchGamesData2() {
+
+      fetchGamesData();
     }
-  }, [savedGameIDs]);
+  }, []);
 
   return (
     <div className="App">
