435a63ca043d16d03a5490d4f2f84b7015174503
Łukasze <lukaszkucharzyk17@gmail.com>
2023-10-19 14:53:37 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 //require("dotenv").config();
 
-const PORT = 80;
+const PORT = 443;
 const HOST = "games-updates-monitor-backend.onrender.com";
 
 const API_URL = "https://store.steampowered.com";
@@ -39,9 +39,9 @@ const proxyOptions2 = {
 const proxy = createProxyMiddleware(proxyOptions);
 const proxy2 = createProxyMiddleware(proxyOptions2);
 
-app.use("/api/", proxy);
-app.use("/ISteamNews/", proxy2);
-app.use("/ISteamApps/", proxy2);
+app.use("/api", proxy);
+app.use("/ISteamNews", proxy2);
+app.use("/ISteamApps", proxy2);
 
 app.listen(HOST, () => {
   console.log(`Proxy Started at ${HOST}`);
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header>
       <div className="container">
-        <h1>Game updates monitor</h1>
+        <h1>Games updates monitor</h1>
       </div>
     </header>
   );
